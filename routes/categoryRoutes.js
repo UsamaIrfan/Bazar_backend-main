23b3dc0043b2dd1d728165f7110abbaee6fbad3f
Faizan Ahmed <faizanahmed@tecizeverything.com>
2021-12-27 23:31:16 +0500

    init

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const {
+  addCategory,
+  addAllCategory,
+  getAllCategory,
+  getCategoryById,
+  updateCategory,
+  updateStatus,
+  deleteCategory,
+} = require('../controller/categoryController');
+
+//add a category
+router.post('/add', addCategory);
+
+//add all category
+router.post('/all', addAllCategory);
+
+//get all category
+router.get('/', getAllCategory);
+
+//get a category
+router.get('/:id', getCategoryById);
+
+//update a category
+router.put('/:id', updateCategory);
+
+//show/hide a category
+router.put('/status/:id', updateStatus);
+
+//delete a category
+router.patch('/:id', deleteCategory);
+
+module.exports = router;
