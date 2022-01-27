e9961a65bae52d0d8da041e0e87ebb9314b03727
Faizan Ahmed <faizanahmed@tecizeverything.com>
2022-01-27 15:20:18 +0500

    Login bug fixed

--- controller/userController.js
+++ controller/userController.js
@@ -68,7 +68,7 @@ const loginUser = async (req, res) => {
 
   } catch (error) {
     res.status(500).send({
-      message: "Network Error",
+      message: "Invalid user or password!",
     });
   }
 
