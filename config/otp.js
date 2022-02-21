9b5e7f49e9ca343cdd0bb0c6d0df2653f8373076
obaid ur rehman <obaidurrehman@tecizeverything.com>
2022-02-21 18:52:19 +0500

    .

---
+++ config/otp.js
@@ -0,0 +1,23 @@
+const Otp = require("twilio");
+
+const sendOtp = async (to, data) => {
+    const { subject, text, html } = data;
+    const client = Otp(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+    const message = await client.messages.create({
+        to,
+        from: process.env.TWILIO_SENDER,
+        subject,
+        text,
+        html
+    })
+        sendOtp.send(message)
+        .then((_response) => {
+            return { success: true }
+        })
+        .catch((_error) => {
+            return { success: false };
+        })
+    
+    };
+
+module.exports = { sendOtp };
